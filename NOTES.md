c636c995c9f0701fe6353f022fa7d793cd708eef
mihalikistvan <mihalikistvan@github.com>
2022-06-12 14:20:34 +0200

    push

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+Pomodoro 1 
+ 
+- written rule for 0 lentgh list 
+- written rule for 1 lentgh list 
+- written rule for 2 lentgh list 
+- written rule for 3 lentgh list 
+- written rule for 3+ lentgh list 
---
+++ test_main.py
@@ -0,0 +1,8 @@
+from main import * 
+
+def test():
+    assert likes([]) == 'no one likes this'
+    assert likes(['Peter']), 'Peter likes this'
+    assert likes(['Jacob', 'Alex'])=='Jacob and Alex like this'
+    assert likes(['Max', 'John', 'Mark'])== 'Max, John and Mark like this'
+    assert likes(['Alex', 'Jacob', 'Mark', 'Max'])== 'Alex, Jacob and 2 others like this'
